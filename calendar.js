f4b874b86e7a4d7c24f0c795d168fa963d62b4d6
Niels van Deursen <nhavandeursen@gmail.com>
2016-01-24 20:15:57 +0100

    included previously removed requires.

--- calendar.js
+++ calendar.js
@@ -1,14 +1,23 @@
-
+var fs = require('fs');
+var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
+
+var SCOPES = ['https://www.googleapis.com/auth/calendar'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
 
 /**
  * Lists the next 10 events on the user's primary calendar.
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function addEvent(auth, task_content, time) {
+function listEvents(auth, task_content, time) {
   var calendar = google.calendar('v3');
+  
+  console.log("Content = " + task_content + " Time = " + time);
   
   var event = {
 	  'summary': task_content,
@@ -38,6 +47,6 @@ module.exports =
 {	
 	AddEvent: function(auth, cont, time)
 	{
-		addEvent(auth, cont, time);
+		listEvents(auth, cont, time);
 	}
 };
